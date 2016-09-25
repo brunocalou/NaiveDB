9bcf24dd961fa8c878d9877a98fe78f3134ef45b
brunocalou <brunocaloualves@gmail.com>
2016-09-25 13:02:03 -0300

    Refactored Table

--- Table.h
+++ Table.h
@@ -32,13 +32,7 @@ struct HeaderFile {
 class Table {
 private:
     static const unsigned HEADER_SIZE = sizeof(RegistryHeader);
-    
-    /**
-     * Hashmap to store the table. It's used only when importing or exporting the
-     * whole table
-     * @see Table::importCSV, Table::exportBin, Table::importBin
-     */
-    map<string, vector<string> > table;
+
     Scheme scheme;
     string name;
     string path;
@@ -83,30 +77,17 @@ public:
      ********** CONVENIENCE METHODS **********
      *****************************************/
      
-    /**
-     * Import a CSV file and convert it to a map of vectors. The first element
-     * is the map key and the others are the elements of the vector. Note that
-     * the table will be stored in memory
-     */
-    void importCSV(const string & path);
+     /**
+      * Read a CSV file, convert and export it to a binary file
+      */
+     void convertFromCSV(const string & path);
     
     /**
-     * Import a table from a binary file to the memory. The binary file must have been generated
-     * using the exportBin method. Note that all the table will be stored in memory
-     * @see Table::exportBin
+     * Print the table binary file (for debugging only)
+     * @param number_of_values the number of values to print. If set to -1
+     *        all the file wil be printed
      */
-    void importBin();
-    
-    /**
-     * Export a table to a binary file. The header is exported for each registry
-     * e.g.: | HEADER | ROW_1_COL_1 | ROW_1_COL_2 | HEADER | ROW_2_COL1 | ROW_2_COL_2
-     * The HEADER is saved as | TABLE_NAME | REGISTRY_SIZE | TIME_STAMP |
-     * Note that the data must have been imported from a CSV file for this method run
-     * correctly because operations like Table::insert will not affect the Table::table variable
-     * @see Table::importCSV
-     * @return true if the export has succeeded, false otherwise
-     */
-    bool exportBin();
+    void print(int number_of_values = -1);
     
     /**
      * Print the header file (for debugging only)
@@ -122,119 +103,12 @@ Table::Table(string name) {
     this->header_file_path = name + "_h.dat";
 }
 
-void Table::importCSV(const string & path) {
-    string line;
-    
-    ifstream file;
-    file.open(path.c_str());
-    
-    
-    if (file.is_open()) {
-        //Header
-        getline(file, line);
-        
-        //Lines
-        while (getline(file, line)) {
-            vector<string> words = split(line, ',');
-            string key = words.at(0);
-            
-            //Erase the key from the vector
-            words.erase(words.begin());
-            
-            //Add the vector to the map
-            table[key] = words; 
-        }
-        file.close();
-    } else {
-        cout << "Unable to open file - " << path << endl;
-    }
-    
-    for (std::map<string, vector<string> >::iterator it = table.begin(); it!=table.end(); ++it) {
-        std::cout << it->first << endl;
-        
-        vector<string> cols = it->second;
-        
-        for (std::vector<string>::iterator v_it = cols.begin(); v_it != cols.end(); v_it++) {
-            cout << "  " << *v_it;
-        }
-        cout << endl;
-    }
-}
-
 void Table::importScheme(const string & path) {
     scheme.import(path);
 }
 
 void Table::setScheme(Scheme scheme) {
     this->scheme = scheme;
-}
-
-void Table::importBin() {
-    ifstream file;
-    file.open(path.c_str(), ios::binary);
-    
-    cout << "Imported" << endl;
-    
-    vector<SchemeCol>* scheme_cols = scheme.getCols();
-    
-    string key;
-    
-    while (!file.eof()) {
-        //Import the header
-        RegistryHeader header;
-        file.read(header.table_name, sizeof(header.table_name));
-        file.read(reinterpret_cast<char *> (& header.registry_size), sizeof(header.registry_size));
-        file.read(reinterpret_cast<char *> (& header.time_stamp), sizeof(header.time_stamp));
-        
-        cout << "  | " << header.table_name << " " << header.registry_size << " " << header.time_stamp << " | ";
-    
-        //Read and convert the values from the file
-        for (vector<SchemeCol>::iterator it = scheme_cols->begin(); it != scheme_cols->end(); it++) {
-            SchemeCol & scheme_col = *it;
-            ostringstream stream;
-            
-            if (scheme_col.type == INT32) {
-                int value;
-                file.read(reinterpret_cast<char *> (&value), scheme_col.getSize());
-                cout << "INT32 " << value << " | ";
-                stream << value;
-            } else if (scheme_col.type == CHAR) {
-                char value[scheme_col.getSize()];
-                file.read(reinterpret_cast<char *> (&value), scheme_col.getSize());
-                cout << "CHAR " << value << " | ";
-                stream << value;
-            } else if (scheme_col.type == FLOAT) {
-                float value;
-                file.read(reinterpret_cast<char *> (&value), scheme_col.getSize());
-                cout << "FLOAT " << value << " | ";
-                stream << value;
-            } else if (scheme_col.type == DOUBLE) {
-                double value;
-                file.read(reinterpret_cast<char *> (&value), scheme_col.getSize());
-                cout << "DOUBLE " << value << " | ";
-                stream << value;
-            }
-            
-            string string_value;
-            string_value = stream.str();
-            
-            //Add the values to the table
-            if (it == scheme_cols->begin()) {
-                key = string_value;
-                vector<string> vec;
-                table[key] = vec;
-            } else {
-                table[key].push_back(string_value);
-            }
-        }
-        
-        //TODO: find where the bug is
-        //the last element is garbage, for some reason. So, delete it
-        // table.erase(key);
-        cout << endl;
-    }
-    
-    file.close();
 }
 
 void Table::convertAndSave(ofstream *file, string * string_value, SchemeCol *scheme_col) {
@@ -258,26 +132,10 @@ void Table::convertAndSave(ofstream *file, string * string_value, SchemeCol *sch
     }
 }
 
-bool Table::exportBin() {
-    cout << "Exported " << endl;
-    
-    for (map<string, vector<string> >::iterator table_it = table.begin(); table_it != table.end(); table_it ++) {
-        //Iterate through the map
-        string key = table_it->first;
-        vector <string> & row = table_it->second;
-        
-        insert(key, row);
-    }
-    
-    return true;
-}
-
 bool Table::insert(string key, vector<string> row) {
     //TODO: create a insert method that receives the file as parameter to improve the performance while adding many rows
     ofstream file;
     file.open(path.c_str(), ios::binary | ios::app);
-    
-    // table[key] = row;
     
     //Save the header position on the header file
     HeaderFile header_file;
@@ -331,12 +189,97 @@ void Table::printHeaderFile(int number_of_values) {
     int counter = 0;
     
     while (!file.eof() && counter != number_of_values) {
-        long long position;
+        HeaderFile header;
+        // The variable position will be the same type of header.registry_position
+        decltype(header.registry_position) position;
         file.read(reinterpret_cast<char *> (&position), sizeof(position));
         cout << position << endl;
         counter ++;
     }
     
     file.close();
+}
+
+void Table::print(int number_of_values) {
+    ifstream file;
+    file.open(path.c_str(), ios::binary);
+    
+    cout << "Print" << endl;
+    
+    vector<SchemeCol>* scheme_cols = scheme.getCols();
+    int counter = 0;
+    
+    while (!file.eof() && counter != number_of_values) {
+        //Import the header
+        RegistryHeader header;
+        file.read(header.table_name, sizeof(header.table_name));
+        file.read(reinterpret_cast<char *> (& header.registry_size), sizeof(header.registry_size));
+        file.read(reinterpret_cast<char *> (& header.time_stamp), sizeof(header.time_stamp));
+        
+        cout << "  | " << header.table_name << " " << header.registry_size << " " << header.time_stamp << " | ";
+    
+        //Read and convert the values from the file
+        for (vector<SchemeCol>::iterator it = scheme_cols->begin(); it != scheme_cols->end(); it++) {
+            SchemeCol & scheme_col = *it;
+            ostringstream stream;
+            
+            if (scheme_col.type == INT32) {
+                int value;
+                file.read(reinterpret_cast<char *> (&value), scheme_col.getSize());
+                cout << "INT32 " << value << " | ";
+                stream << value;
+            } else if (scheme_col.type == CHAR) {
+                char value[scheme_col.getSize()];
+                file.read(reinterpret_cast<char *> (&value), scheme_col.getSize());
+                cout << "CHAR " << value << " | ";
+                stream << value;
+            } else if (scheme_col.type == FLOAT) {
+                float value;
+                file.read(reinterpret_cast<char *> (&value), scheme_col.getSize());
+                cout << "FLOAT " << value << " | ";
+                stream << value;
+            } else if (scheme_col.type == DOUBLE) {
+                double value;
+                file.read(reinterpret_cast<char *> (&value), scheme_col.getSize());
+                cout << "DOUBLE " << value << " | ";
+                stream << value;
+            }
+            
+            string string_value;
+            string_value = stream.str();
+        }
+        cout << endl;
+        counter ++;
+    }
+    
+    file.close();
+}
+
+void Table::convertFromCSV(const string & path) {
+    string line;
+    
+    ifstream file;
+    file.open(path.c_str());
+    
+    
+    if (file.is_open()) {
+        //Header
+        getline(file, line);
+        
+        //Lines
+        while (getline(file, line)) {
+            vector<string> words = split(line, ',');
+            string key = words.at(0);
+            
+            //Erase the key from the vector
+            words.erase(words.begin());
+            
+            // Insert the line on the database
+            insert(key, words);
+        }
+        file.close();
+    } else {
+        cout << "Unable to open file - " << path << endl;
+    }
 }
 #endif //TABLE_H
--- main.cpp
+++ main.cpp
@@ -6,10 +6,9 @@ using namespace std;
 int main() {    
     //Import the csv
     Table table("Aluno");
-    // table.importCSV("alunos.csv");
     table.importScheme("alunos_esquema.txt");
-    // table.exportBin();
-    // table.importBin();
+    // table.convertFromCSV("alunos.csv");
+    table.print(5);
     table.printHeaderFile(5);
     
     return 0;
